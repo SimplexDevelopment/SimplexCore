628294f148d1d2fbc61e04c97edd33bf067ab921
abhiram <abhithegamer2019@gmail.com>
2021-03-26 23:37:30 +0530

    Update on Structure

--- src/main/java/io/github/simplexdev/simplexcore/structures/Structure.java
+++ src/main/java/io/github/simplexdev/simplexcore/structures/Structure.java
@@ -1,5 +1,45 @@
 package io.github.simplexdev.simplexcore.structures;
 
-public class Structure {
-    // TODO: Write this
+import io.github.simplexdev.simplexcore.module.SimplexModule;
+import io.github.simplexdev.simplexcore.structures.block.NBTBlock;
+import io.github.simplexdev.simplexcore.structures.exception.InvalidSchematic;
+import io.github.simplexdev.simplexcore.structures.exception.SchematicNotLoaded;
+import org.bukkit.Location;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Vector;
+
+public final class Structure {
+    public Plugin plugin;
+    private File schematic;
+    private short width = 0;
+    private short height = 0;
+    private short length = 0;
+    private byte[] blockdatacontainer;
+
+    private HashMap<Vector, NBTBlock> nbtBlocks = new HashMap<>();
+    private HashMap<Integer, BlockData> blocks = new HashMap<>();
+
+    public Structure(SimplexModule<?> plugin,File schematic)
+    {
+        this.plugin = plugin.getPlugin();
+        this.schematic = schematic;
+    }
+
+    public void load() throws InvalidSchematic
+    {
+
+    }
+
+    public void paste(Location loc,PasteType pasteType) throws SchematicNotLoaded
+    {
+        if (width == 0 || height == 0 || length == 0 || blocks.isEmpty()) {
+            throw new SchematicNotLoaded("Schematic not loaded please load schematic first...");
+        }
+
+
+    }
 }
---
+++ src/main/java/io/github/simplexdev/simplexcore/structures/exception/SchematicNotLoaded.java
@@ -0,0 +1,8 @@
+package io.github.simplexdev.simplexcore.structures.exception;
+
+public class SchematicNotLoaded extends Exception {
+
+    public SchematicNotLoaded(String msg) {
+        super(msg);
+    }
+}
